cb97f313977d6ea40bb2b0216e28a78a4b7c7a77
Benedek Kupper <kupper.benedek@gmail.com>
2018-06-28 21:50:06 +0200

    Add BSP for STM32L4 series

---
+++ BSP_STM32L4xx/bsp_io.c
@@ -0,0 +1,39 @@
+/**
+  ******************************************************************************
+  * @file    bsp_io.c
+  * @author  Benedek Kupper
+  * @version 0.1
+  * @date    2018-05-21
+  * @brief   DfuBootloader BSP for I/O pins
+  *
+  * Copyright (c) 2018 Benedek Kupper
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+#include <bsp_io.h>
+
+const GPIO_InitType BSP_IOCfg[] =
+{
+    /* USB pins */
+    {
+        .Mode = GPIO_MODE_ALTERNATE,
+        .Pull = GPIO_PULL_FLOAT,
+        .Output.Type  = GPIO_OUTPUT_PUSHPULL,
+        .Output.Speed = VERY_HIGH,
+#if defined(USB)
+        .AlternateMap = GPIO_USB_AF10,
+#elif defined(USB_OTG_FS)
+        .AlternateMap = GPIO_OTG_FS_AF10,
+#endif
+    },
+};
---
+++ BSP_STM32L4xx/bsp_system.c
@@ -0,0 +1,73 @@
+/**
+  ******************************************************************************
+  * @file    bsp_system.c
+  * @author  Benedek Kupper
+  * @version 0.1
+  * @date    2018-05-21
+  * @brief   DfuBootloader BSP for system clocking
+  *
+  * Copyright (c) 2018 Benedek Kupper
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+#include <bsp_system.h>
+#include <xpd_rcc.h>
+#ifdef RCC_HSI48_SUPPORT
+#include <xpd_crs.h>
+
+static const CRS_InitType crsSetup = {
+    .Source     = CRS_SYNC_SOURCE_USB,
+    .ErrorLimit = CRS_ERRORLIMIT_DEFAULT,
+};
+#endif
+
+static const RCC_MSI_InitType msiconf = {
+    .ClockFreq = MSI_48MHz,
+    .State = ENABLE
+};
+
+static const RCC_PLL_InitType pllconf = {
+    .State = ENABLE,
+    .Source = MSI,
+    .M = 6,
+    .N = 20,
+    .R = 2, /* MSI / 6 * 20 / 2 = PLLR = 80000000 -> SYSCLK */
+
+    .Q = 2,
+    .P = 7
+};
+
+/* System clocks configuration */
+void SystemClock_Config(void)
+{
+    RCC_eMSI_Config(&msiconf);
+
+#if (LSE_VALUE_Hz == 32768)
+    /* Use LSE to synchronize MSI */
+    RCC_eLSE_Config(OSC_ON);
+#endif
+
+    RCC_ePLL_Config(&pllconf);
+
+#ifdef RCC_HSI48_SUPPORT
+    /* HSI48 configuration */
+    RCC_eHSI48_Enable();
+    CRS_vInit(&crsSetup);
+#endif
+
+    /* System clocks configuration */
+    RCC_eHCLK_Config(PLL, CLK_DIV1, 4);
+
+    RCC_vPCLK1_Config(CLK_DIV1);
+    RCC_vPCLK2_Config(CLK_DIV1);
+}
